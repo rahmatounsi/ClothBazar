e765a8043d9b08e5e413e47b6c9454e38bcde6a3
rahma <rahmatounsi1302@gmail.com>
2018-10-17 15:54:51 +0200

    project Initialed

---
+++ ClothBazar.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazar.web.Startup))]
+namespace ClothBazar.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
